ea7abbb52f427b97b2d4030974832ecc24b876a5
cfurmykola-ctrl <c.fur.mykola@student.uzhnu.edu.ua>
2026-04-15 19:29:43 +0300

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System.Windows;
+using System.Windows;
 
-namespace KnowledgeMap {
+namespace PRK2 {
     public partial class App : Application {
+        public static Models.User CurrentUser = new Models.User();
     }
 }
